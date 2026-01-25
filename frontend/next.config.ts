e1be46630e6ae93051307d4d295bb9b7ee584ff4
yupeyo <bek9490@gmail.com>
2026-01-25 11:03:27 +0700

    Switch to automatic build mode

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // output: 'standalone'을 삭제하여 Amplify 자동 어댑터가 작동하게 합니다.
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true, // 이미지 에러 방지를 위해 이것만 남겨둡니다.
   },
 };
 
